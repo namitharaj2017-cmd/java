c83ddad04d639813166a4658aa1cc149adfb8c0f
namitharaj2017-cmd <namitharaj2017@gmail.com>
2026-03-11 20:45:00 +0530

    Add addition method to Test class

--- Test.java
+++ Test.java
@@ -1,4 +1,9 @@
-
+public class Test {
+ // Addition
+    static void add(int a, int b) {
+        int res = a + b;
+        System.out.println("Addition result: " + res);
+    }
 
     // Subtraction
     static void sub(int a, int b) {
